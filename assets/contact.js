ddc8b61d8a21e663aad88f88a1cfd60dd8836af2
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2021-05-07 13:32:51 -0600

    Replace var with const in contact script

--- assets/contact.js
+++ assets/contact.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var form = document.getElementById('contact-form');
-var message = document.getElementById('message');
+const form = document.getElementById('contact-form');
+const message = document.getElementById('message');
 
-function checkLimit(e) {
-  var length = this.value.length;
-  var error = document.getElementById('error');
-  var counter = error.getElementsByTagName('span').item(0);
-  var button = form.getElementsByTagName('button').item(0);
+function checkLimit() {
+  const length = this.value.length;
+  const error = document.getElementById('error');
+  const counter = error.getElementsByTagName('span').item(0);
+  const button = form.getElementsByTagName('button').item(0);
 
   if (length > this.charLimit) {
     counter.innerHTML = length;
