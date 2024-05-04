8e2de8db52893040ba6ad143e188ac2d8c343fb0
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2024-05-03 21:38:35 -0600

    Upgrade to reCAPTCHA v3

--- assets/contact.js
+++ assets/contact.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const form = document.getElementById('contact-form');
 const message = document.getElementById('message');
 
 function checkLimit() {
   const length = this.value.length;
   const error = document.getElementById('error');
   const counter = error.getElementsByTagName('span').item(0);
-  const button = form.getElementsByTagName('button').item(0);
+  const button = document.querySelector('form button');
 
   if (length > this.charLimit) {
     counter.innerHTML = length;
@@ -19,16 +18,9 @@ function checkLimit() {
   }
 }
 
-function beforeSubmit(e) {
-  e.preventDefault();
-  grecaptcha.execute();
+function onSubmit(token) {
+  document.getElementById('contact-form').submit();
 }
-
-function submit(token) {
-  form.submit();
-}
-
-form.addEventListener('submit', beforeSubmit);
 
 message.charLimit = 1000;
 message.addEventListener('keyup', checkLimit);
